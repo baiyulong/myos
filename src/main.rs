010ad2ebf140e6c4aebde7b9d9385db717941671
baiyulong <baiyulong2009@hotmail.com>
2022-02-12 15:25:05 +0800

    Build a VGA buffer to print message

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,20 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("The VGA text mode is a simple way to print text to the screen. In this post, we create an interface that makes its usage safe and simple, by encapsulating all unsafety in a separate module. We also implement support for Rust’s formatting macros.");
 
     loop {}
 }
