6b1602aeb176b27450d7f1b4d0fc8908792b0113
baiyulong <baiyulong2009@hotmail.com>
2022-02-13 16:28:02 +0800

    Build testing environments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(myos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use myos::println;
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    myos::test_panic_handler(info)
 }
 
 #[no_mangle] // don't mangle the name of this function
@@ -18,5 +27,13 @@ pub extern "C" fn _start() -> ! {
     // named `_start` by default
     println!("The VGA text mode is a simple way to print text to the screen. In this post, we create an interface that makes its usage safe and simple, by encapsulating all unsafety in a separate module. We also implement support for Rust’s formatting macros.");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(0, 1);
 }
